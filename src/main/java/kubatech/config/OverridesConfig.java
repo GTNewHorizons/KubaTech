085a51e6dbeff4329f4512a061ce5cb475804967
kuba6000 <kuba.123123.6000@gmail.com>
2022-08-22 20:14:28 +0200

    GTNHCoreMod custom drops integration

--- src/main/java/kubatech/config/OverridesConfig.java
+++ src/main/java/kubatech/config/OverridesConfig.java
@@ -1,8 +1,11 @@
 package kubatech.config;
 
+import com.dreammaster.main.MainRegistry;
+import com.dreammaster.modcustomdrops.CustomDrops;
 import com.google.common.io.Files;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import cpw.mods.fml.common.registry.GameRegistry;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import java.io.File;
@@ -12,10 +15,16 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 import kubatech.Tags;
 import kubatech.api.ConstructableItemStack;
+import kubatech.api.LoaderReference;
 import kubatech.api.mobhandler.MobDrop;
 import kubatech.api.utils.GSONUtils;
+import kubatech.api.utils.ReflectionHelper;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.JsonToNBT;
+import net.minecraft.nbt.NBTTagCompound;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -109,6 +118,54 @@ public class OverridesConfig {
             reader = Files.newReader(overrideFile, StandardCharsets.UTF_8);
             overrides = gson.fromJson(reader, new TypeToken<Map<String, MobOverride>>() {}.getType());
             overrides.remove("ExampleMob");
+            if (LoaderReference.GTNHCoreMod) {
+                CustomDrops coredrops =
+                        ReflectionHelper.getField(MainRegistry.Module_CustomDrops, "_mCustomDrops", null);
+                if (coredrops != null) {
+                    @SuppressWarnings("unchecked")
+                    ArrayList<CustomDrops.CustomDrop> customdrops = (ArrayList<CustomDrops.CustomDrop>)
+                            ((ArrayList<CustomDrops.CustomDrop>) coredrops.getCustomDrops()).clone();
+                    for (CustomDrops.CustomDrop customdrop : customdrops) {
+                        try {
+                            Class<?> eclass = Class.forName(customdrop.getEntityName());
+                            if (!EntityLiving.class.isAssignableFrom(eclass)) continue;
+                            String ename = (String) EntityList.classToStringMapping.get(eclass);
+                            if (ename == null) continue;
+                            MobOverride override = overrides.computeIfAbsent(ename, k -> new MobOverride());
+                            for (CustomDrops.CustomDrop.Drop drop : customdrop.getDrops()) {
+                                GameRegistry.UniqueIdentifier UI =
+                                        new GameRegistry.UniqueIdentifier(drop.getItemName());
+                                ItemStack stack = GameRegistry.findItemStack(UI.modId, UI.name, 1);
+                                if (stack == null) continue;
+                                String pNBT = ReflectionHelper.getField(drop, "mTag", null);
+                                if (pNBT != null && !pNBT.isEmpty()) {
+                                    try {
+                                        stack.stackTagCompound = (NBTTagCompound) JsonToNBT.func_150315_a(pNBT);
+                                    } catch (Exception ignored) {
+                                    }
+                                }
+                                int chance = drop.getChance() * 100;
+                                int amount = drop.getAmount();
+                                if (drop.getIsRandomAmount()) {
+                                    // average chance formula
+                                    // chance *= ((((amount * (amount + 1d)) / 2d)) + 1d) / (amount + 1d);
+                                    chance *= (2d + (amount * amount) + amount) / (2d * (amount + 1d));
+                                    amount = 1;
+                                    if (chance > 10000) {
+                                        int div = (int) Math.ceil(chance / 10000d);
+                                        amount *= div;
+                                        chance /= div;
+                                    }
+                                }
+                                stack.stackSize = amount;
+                                override.additions.add(new MobDrop(stack, MobDrop.DropType.Normal, chance, null, null));
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
