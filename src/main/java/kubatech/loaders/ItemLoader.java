5cd0b18d5e7988693510eeb28ad81e6bca60f9ff
kuba6000 <kuba.123123.6000@gmail.com>
2022-08-28 03:50:32 +0200

    Move recipes

--- src/main/java/kubatech/loaders/ItemLoader.java
+++ src/main/java/kubatech/loaders/ItemLoader.java
@@ -22,18 +22,11 @@ package kubatech.loaders;
 import static kubatech.api.enums.ItemList.*;
 
 import cpw.mods.fml.common.registry.GameRegistry;
-import gregtech.api.enums.GT_Values;
-import gregtech.api.util.GT_Utility;
-import gtPlusPlus.core.lib.CORE;
-import kubatech.api.LoaderReference;
 import kubatech.loaders.item.KubaItems;
 import kubatech.loaders.item.items.Tea;
 import kubatech.loaders.item.items.TeaCollection;
 import kubatech.loaders.item.items.TeaIngredient;
 import kubatech.loaders.item.items.TeaUltimate;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.fluids.FluidRegistry;
-import net.minecraftforge.fluids.FluidStack;
 
 public class ItemLoader {
     public static final KubaItems kubaitems = new KubaItems();
@@ -82,121 +75,5 @@ public class ItemLoader {
         FermentedTeaLeaf.set(kubaitems.registerProxyItem(new TeaIngredient("fermented_tea_leaf")));
         BruisedTeaLeaf.set(kubaitems.registerProxyItem(new TeaIngredient("bruised_tea_leaf")));
         PartiallyOxidizedTeaLeaf.set(kubaitems.registerProxyItem(new TeaIngredient("partially_oxidized_tea_leaf")));
-
-        //    TEA LINE    //
-        if (LoaderReference.GTPlusPlus && LoaderReference.HarvestCraft) {
-            CORE.RA.addDehydratorRecipe(
-                    new ItemStack[] {GameRegistry.findItemStack("harvestcraft", "tealeafItem", 1)},
-                    null,
-                    null,
-                    new ItemStack[] {TeaLeafDehydrated.get(1)},
-                    null,
-                    100,
-                    32);
-            CORE.RA.addDehydratorRecipe(
-                    new ItemStack[] {TeaLeafDehydrated.get(1)},
-                    null,
-                    null,
-                    new ItemStack[] {WhiteTeaLeaf.get(1)},
-                    null,
-                    100,
-                    32);
-            GT_Values.RA.addMixerRecipe(
-                    new ItemStack[] {TeaLeafDehydrated.get(1)},
-                    new FluidStack[] {FluidRegistry.getFluidStack("water", 50)},
-                    new ItemStack[] {SteamedTeaLeaf.get(1)},
-                    null,
-                    100,
-                    32);
-            CORE.RA.addDehydratorRecipe(
-                    new ItemStack[] {SteamedTeaLeaf.get(1)},
-                    null,
-                    null,
-                    new ItemStack[] {YellowTeaLeaf.get(1)},
-                    null,
-                    100,
-                    32);
-            GT_Values.RA.addBenderRecipe(TeaLeafDehydrated.get(1), RolledTeaLeaf.get(1), 100, 32);
-            CORE.RA.addDehydratorRecipe(
-                    new ItemStack[] {RolledTeaLeaf.get(1)},
-                    null,
-                    null,
-                    new ItemStack[] {GreenTeaLeaf.get(1)},
-                    null,
-                    100,
-                    32);
-            GT_Values.RA.addChemicalRecipe(
-                    RolledTeaLeaf.get(1), GT_Utility.getIntegratedCircuit(1), OxidizedTeaLeaf.get(1), 100, 32);
-            CORE.RA.addDehydratorRecipe(
-                    new ItemStack[] {OxidizedTeaLeaf.get(1)},
-                    null,
-                    null,
-                    new ItemStack[] {BlackTeaLeaf.get(1)},
-                    null,
-                    100,
-                    32);
-            GT_Values.RA.addChemicalRecipe(
-                    RolledTeaLeaf.get(1), GT_Utility.getIntegratedCircuit(2), FermentedTeaLeaf.get(1), 200, 32);
-            CORE.RA.addDehydratorRecipe(
-                    new ItemStack[] {FermentedTeaLeaf.get(1)},
-                    null,
-                    null,
-                    new ItemStack[] {PuerhTeaLeaf.get(1)},
-                    null,
-                    100,
-                    32);
-            GT_Values.RA.addCutterRecipe(
-                    new ItemStack[] {TeaLeafDehydrated.get(1)},
-                    new ItemStack[] {BruisedTeaLeaf.get(1)},
-                    100,
-                    32,
-                    false);
-            GT_Values.RA.addChemicalRecipe(
-                    BruisedTeaLeaf.get(1), GT_Utility.getIntegratedCircuit(1), PartiallyOxidizedTeaLeaf.get(1), 50, 32);
-            CORE.RA.addDehydratorRecipe(
-                    new ItemStack[] {PartiallyOxidizedTeaLeaf.get(1)},
-                    null,
-                    null,
-                    new ItemStack[] {OolongTeaLeaf.get(1)},
-                    null,
-                    100,
-                    32);
-
-            // Tea Assembly
-            GameRegistry.addSmelting(BlackTeaLeaf.get(1), BlackTea.get(1), 10);
-            GT_Values.RA.addMixerRecipe(
-                    new ItemStack[] {BlackTea.get(1), GameRegistry.findItemStack("harvestcraft", "limejuiceItem", 1)},
-                    null,
-                    new ItemStack[] {EarlGrayTea.get(1)},
-                    null,
-                    100,
-                    32);
-            GameRegistry.addSmelting(GreenTeaLeaf.get(1), GreenTea.get(1), 10);
-            GT_Values.RA.addMixerRecipe(
-                    new ItemStack[] {BlackTea.get(1)},
-                    new FluidStack[] {FluidRegistry.getFluidStack("potion.lemonjuice", 1000)},
-                    new ItemStack[] {LemonTea.get(1)},
-                    null,
-                    100,
-                    32);
-            GT_Values.RA.addMixerRecipe(
-                    new ItemStack[] {BlackTea.get(1)},
-                    new FluidStack[] {FluidRegistry.getFluidStack("milk", 1000)},
-                    new ItemStack[] {MilkTea.get(1)},
-                    null,
-                    100,
-                    32);
-            GameRegistry.addSmelting(OolongTeaLeaf.get(1), OolongTea.get(1), 10);
-            GT_Values.RA.addMixerRecipe(
-                    new ItemStack[] {GameRegistry.findItemStack("harvestcraft", "peppermintItem", 1)},
-                    new FluidStack[] {FluidRegistry.getFluidStack("water", 1000)},
-                    new ItemStack[] {PeppermintTea.get(1)},
-                    null,
-                    100,
-                    32);
-            GameRegistry.addSmelting(PuerhTeaLeaf.get(1), PuerhTea.get(1), 10);
-            GameRegistry.addSmelting(WhiteTeaLeaf.get(1), WhiteTea.get(1), 10);
-            GameRegistry.addSmelting(YellowTeaLeaf.get(1), YellowTea.get(1), 10);
-        }
     }
 }
